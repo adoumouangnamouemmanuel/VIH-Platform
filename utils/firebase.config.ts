3e5308cc886f091d7a6c4c5e335b5a9f9ed910bb
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2025-06-12 23:12:27 +0000

    added firebase configuration

---
+++ utils/firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyALpY56MtJKkjYG4wbzMT7kSO0yb4lNUOE",
+  authDomain: "hiv-platform-e54e8.firebaseapp.com",
+  projectId: "hiv-platform-e54e8",
+  storageBucket: "hiv-platform-e54e8.firebasestorage.app",
+  messagingSenderId: "1037118682430",
+  appId: "1:1037118682430:web:27f9d7522504246e19686f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
